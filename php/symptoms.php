37d069b87961501491b01a2885d7546e010f2fc1
PatilRohitRP <rohit.patil.rrp16@gmail.com>
2018-03-13 20:02:33 +0530

    new

--- php/symptoms.php
+++ php/symptoms.php
@@ -2,6 +2,13 @@
 <html>
 <head>
     <title>Bootstrap Autocomplete with Dynamic Data Load using PHP Ajax</title>
+    <link rel="stylesheet" type="text/css" href="../css/login.css">
+<link rel="stylesheet" type="text/css" href="../css/index.css">
+<link rel="stylesheet" type="text/css" href="../css/animate.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
+<link rel="icon" type="image/png" href="../images/kisaanlogo.png" sizes="96x96">
+<script type="JavaScript"></script>
   <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
     <script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
     <script type="text/javascript" src="typeahead.js"></script>
@@ -21,9 +28,34 @@
   </style>  
 </head>
 <body>
-  <div class="bgcolor">
+  <div style="background: linear-gradient(rgba(26,26,26,0.2),rgba(26,26,26,0.7)),url('../images/background6.jpg ');background-size:contain,cover;background-attachment:fixed;">
+<a href="javascript:" id="return-to-top"><i class="icon-chevron-up"></i></a>
+
+  <div class="header animated fadeInUp" style="height:70px;background:transparent; ">
+ <p style="font-size:20px;color:white"><i class="fa fa-stethoscope"></i>  MediCare</p>
+ <div id="google_translate_element" style="margin-left:1170px;"></div>
+
+<script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+}
+</script>
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+ <ul style="margin-left:1100px;"> 
+  <li><a href="index.php"><i class="fa fa-home"></i>&nbsp;Home</a></li>
+   <li><a href="contact.php"><i class="fa fa-phone"></i>&nbsp;Contact</a></li>
+ </div>
+  <div class="container">
     <label class="demo-label">Enter The Symptoms : </label><br/> <form method="POST"><input type="text" name="txtCountry" id="txtCountry" class="typeahead"/></form>
-  </div>
+  </div><br><br><br><br>
+  <div class="Contact" id="contact1">
+<p style="color:white;">
+<img src="../images/toll.png" width="0px"><i class="fa fa-phone"></i>&nbsp;Toll Free 1800-274-3131
+<img src="../images/email.png" width="0px"><i class="fa fa-envelope"></i>&nbsp;MediCare@service.com
+<img src="../images/franchize.png" width="0px"><i class="fa fa-handshake-o"></i>&nbsp;Franchise
+</p>
+<h4 >Copyright © 2018 MediCare.com. All rights reserved.</h4>
+</div>
 </body>
 <script>
     $(document).ready(function () {
